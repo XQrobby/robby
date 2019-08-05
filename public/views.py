6f9b780592f60bf90b131ef032e2d305469d5807
Robby <1260777400@qq.com>
2019-08-05 10:46:45 +0800

    调试

--- public/views.py
+++ public/views.py
@@ -55,10 +55,17 @@ def develop(request):
         return JsonResponse({"status":False})
     elif request.method == 'POST':
         print(request.POST)
-        try:
-            response.autoreply(request)
-        except Exception as e:
-            print(e.args)
+        msg = parse_message(request.body)
+        if msg.type == 'text':
+            reply = create_reply('这是条文字消息', msg)
+        elif msg.type == 'image':
+            reply = create_reply('这是条图片消息', msg)
+        elif msg.type == 'voice':
+            reply = create_reply('这是条语音消息', msg)
+        else:
+            reply = create_reply('这是条其他类型消息', msg)
+        response = HttpResponse(reply.render(), content_type="application/xml")
+        return response
 
 def access_token(request):
     access_token = query.use_access_token()
