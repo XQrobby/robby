73e3448288d6d9d5cfc33ee7f7f36a5226d34dd3
Robby <1260777400@qq.com>
2019-08-05 10:34:02 +0800

    调试

--- public/response.py
+++ public/response.py
@@ -3,13 +3,13 @@ from django.http.response import HttpResponse,JsonResponse
 
 def autoreply(request):
     msg = parse_message(request.body)
-        if msg.type == 'text':
-            reply = create_reply('这是条文字消息', msg)
-        elif msg.type == 'image':
-            reply = create_reply('这是条图片消息', msg)
-        elif msg.type == 'voice':
-            reply = create_reply('这是条语音消息', msg)
-        else:
-            reply = create_reply('这是条其他类型消息', msg)
-        response = HttpResponse(reply.render(), content_type="application/xml")
-        return response
+    if msg.type == 'text':
+        reply = create_reply('这是条文字消息', msg)
+    elif msg.type == 'image':
+        reply = create_reply('这是条图片消息', msg)
+    elif msg.type == 'voice':
+        reply = create_reply('这是条语音消息', msg)
+    else:
+        reply = create_reply('这是条其他类型消息', msg)
+    response = HttpResponse(reply.render(), content_type="application/xml")
+    return response
