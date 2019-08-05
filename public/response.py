d5c2d9d6c574d0ab3d2c73a43b4a0f26528ab8a0
Robby <1260777400@qq.com>
2019-08-05 09:25:42 +0800

    调试

--- public/response.py
+++ public/response.py
@@ -3,6 +3,7 @@ from django.http.response import HttpResponse,JsonResponse
 
 def autoreply(request):
     try:
+        print(request.body)
         msg = parse_message(request.body)
         if msg.type == 'text':
             reply = create_reply('这是条文字消息',msg)
@@ -12,9 +13,9 @@ def autoreply(request):
             reply = create_reply('这是条语音消息',msg)
         else:
             reply = create_reply('这是条其他类型消息',msg)
-        response = HttpResponse(reply.render(),content_type='application/xml')
-        return response
+        #response = HttpResponse(reply.render(),content_type='application/xml')
+        #return response
     except:
         reply = create_reply('---------',msg)  
-        response = HttpResponse(reply.render(),content_type='application/xml')
-        return response
+        #response = HttpResponse(reply.render(),content_type='application/xml')
+        #return response
