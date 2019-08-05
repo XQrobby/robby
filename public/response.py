c8b24c419a2141e0e0f95ede1244662cb97e9e2e
Robby <1260777400@qq.com>
2019-08-05 09:31:40 +0800

    调试

--- public/response.py
+++ public/response.py
@@ -7,12 +7,16 @@ def autoreply(request):
         msg = parse_message(request.body)
         if msg.type == 'text':
             reply = create_reply('这是条文字消息',msg)
+            print('这是条文字消息')
         elif msg.type == 'image':
             reply = create_reply('这是条图片消息',msg)
+            print('这是条图片消息')
         elif msg.type == 'voice':
             reply = create_reply('这是条语音消息',msg)
+            print('这是条语音消息')
         else:
             reply = create_reply('这是条其他类型消息',msg)
+            print('这是条其他类型消息')
         #response = HttpResponse(reply.render(),content_type='application/xml')
         #return response
     except:
