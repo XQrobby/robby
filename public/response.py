c903551117de33fb1cf118da3b6c84a86c2f75d3
Robby <1260777400@qq.com>
2019-08-05 09:17:46 +0800

    自动回复调试

--- public/response.py
+++ public/response.py
@@ -12,9 +12,9 @@ def autoreply(request):
             reply = create_reply('这是条语音消息',msg)
         else:
             reply = create_reply('这是条其他类型消息',msg)
-        response = HttpResponse(reply.render(),context_type='application/xml')
+        response = HttpResponse(reply.render(),content_type='application/xml')
         return response
     except:
         reply = create_reply('---------',msg)  
-        response = HttpResponse(reply.render(),context_type='application/xml')
+        response = HttpResponse(reply.render(),content_type='application/xml')
         return response
--- public/views.py
+++ public/views.py
@@ -32,7 +32,7 @@ def wechat(request):
             reply = create_reply('这是条语音消息',msg)
         else:
             reply = create_reply('这是条其他类型消息',msg)
-        response = HttpResponse(reply.render(),context_type='application/xml')
+        response = HttpResponse(reply.render(),content_type='application/xml')
         return response
     else:
         logger.info('-----------------')    
