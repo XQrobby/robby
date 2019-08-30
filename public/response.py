d8458b104f25978b9d1a36e09d3158fb8d213d50
Robby <1260777400@qq.com>
2019-08-30 11:25:48 +0800

    测试

--- public/response.py
+++ public/response.py
@@ -4,6 +4,7 @@ from json import dumps
 def autoreply(request):
     msg = parse_message(request.body)
     reply = create_reply('',msg)
+    print("type:%s/n"(msg.type),dir(msg))
     if msg.event:
         if msg.event == 'subscribe':
             reply = create_reply('感谢关注',msg)
