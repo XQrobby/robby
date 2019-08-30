54264c94df1cd6f2335f5f6e24b8629f2c066fcf
Robby <1260777400@qq.com>
2019-08-30 11:36:15 +0800

    测试

--- public/response.py
+++ public/response.py
@@ -13,7 +13,7 @@ def autoreply(request):
         reply = create_reply('这是条语音消息', msg)
     elif msg.type == 'event':
         if msg.event == 'subscribe':
-            reply = create_reply('感谢关注'，msg)
+            reply = create_reply('感谢关注',msg)
     else:
         reply = create_reply('这是条其他类型消息', msg)
     response = HttpResponse(reply.render(), content_type="application/xml")
