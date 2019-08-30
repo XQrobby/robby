22d63bb52acda70e0f26240230d80e5a84481b52
Robby <1260777400@qq.com>
2019-08-30 11:33:12 +0800

    测试

--- public/response.py
+++ public/response.py
@@ -11,6 +11,8 @@ def autoreply(request):
         reply = create_reply('这是条图片消息', msg)
     elif msg.type == 'voice':
         reply = create_reply('这是条语音消息', msg)
+    elif msg.type == 'event':
+        print(msg.event)
     else:
         reply = create_reply('这是条其他类型消息', msg)
     response = HttpResponse(reply.render(), content_type="application/xml")
