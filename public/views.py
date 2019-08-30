b98873c3b9ff05d889aba85d1d981a81b66ca319
Robby <1260777400@qq.com>
2019-08-30 11:01:59 +0800

    测试

--- public/views.py
+++ public/views.py
@@ -56,6 +56,7 @@ def develop(request):
         return JsonResponse({"status":False})
     elif request.method == 'POST':
         print(request.body)
+        '''
         msg = parse_message(request.body)
         if msg.type == 'text':
             reply = create_reply('这是条文字消息', msg)
@@ -66,6 +67,8 @@ def develop(request):
         else:
             reply = create_reply('这是条其他类型消息', msg)
         response = HttpResponse(reply.render(), content_type="application/xml")
+        '''
+        response = rspon.autoreply(request)
         return response
 
 def access_token(request):
