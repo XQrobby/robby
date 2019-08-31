cd2d3da80cf0a0e6267146e247df5cb7d6d9586f
Robby <1260777400@qq.com>
2019-08-31 09:24:30 +0800

    测试

--- public/views.py
+++ public/views.py
@@ -55,7 +55,7 @@ def develop(request):
             return HttpResponse(content['echostr'])
         return JsonResponse({"status":False})
     elif request.method == 'POST':
-        print('body:',request.body,'\npost:',dir(request),request.parse_file_upload)
+        print('body:',request.body,'\npost:',dir(request),request.get_full_path_info)
         response = rspon.autoreply(request)
         return response
 
