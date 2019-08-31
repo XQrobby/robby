bee4272d0364cd6d18e75bbc7c7c2c84cf4f5cee
Robby <1260777400@qq.com>
2019-08-31 09:33:10 +0800

    测试

--- public/views.py
+++ public/views.py
@@ -55,7 +55,7 @@ def develop(request):
             return HttpResponse(content['echostr'])
         return JsonResponse({"status":False})
     elif request.method == 'POST':
-        print('body:',request.body,'\npost:',dir(request),request.COOKIES)
+        print('body:',request.body,'\npost:',dir(request),request.META)
         response = rspon.autoreply(request)
         return response
 
