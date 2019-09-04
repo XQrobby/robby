6326a12c1d14b176b87e74bf941bbaacf70ca231
Robby <1260777400@qq.com>
2019-09-04 21:55:13 +0800

    admin更新

--- public/admin.py
+++ public/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import ScholarUser,App
 # Register your models here.
-admin.site.register(ScholarUser)
+
+class ScholarUserAdmin:
+    list_display = ['name','tel','division','activation']
+
+admin.site.register(ScholarUser,ScholarUserAdmin)
 admin.site.register(App)
