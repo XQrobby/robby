7f8ac26371be745bf9d3f5ee29a98278dd85ee8e
Robby <1260777400@qq.com>
2019-09-10 13:21:19 +0800

    调试

--- public/admin.py
+++ public/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import ScholarUser,App
+from django.utils.html import format_html
 # Register your models here.
 
 def user_activate(modeladmin,request,queryset):
