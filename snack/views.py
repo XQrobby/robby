ecc4d76a16ac21f2178af9462b858aff5167e3b5
Robby <1260777400@qq.com>
2019-09-16 23:10:26 +0800

    appid调试

--- snack/views.py
+++ snack/views.py
@@ -11,8 +11,8 @@ def login(request):
         #获取unionCode
         api = 'https://api.weixin.qq.com/sns/jscode2session'
         payload = {
-            'appid':'wx10a71b42870fba91',
-            'secret':'f725c0da9b3b41f8a8b6c98c57e32e7b',
+            'appid':'wxb0b3a42da4af0d73',
+            'secret':'5383a21d1b21dcb4f6bf1309a2c2b429',
             'js_code':request.POST.get('code'),
             'grant_type':'authorization_code'
             }
