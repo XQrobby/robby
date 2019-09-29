93cf0068789b10133e809775a9d4e52fa37cfccf
Robby <1260777400@qq.com>
2019-09-29 17:33:44 +0800

    execel

--- snack/admin.py
+++ snack/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import ServiceType,Order,Division,Image,Client
 from django.utils.html import format_html
+from openpyxl import Workbook
+from django.http.response import HttpResponse
 # Register your models here.
-
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['orderID','createTime','typ','name','tel','technician','is_assess','orderStatus','serviceStatus','Photo','buttons']
     ordering = ['-createTime']
@@ -13,7 +14,7 @@ class OrderAdmin(admin.ModelAdmin):
         ("订单内容", {'fields':[ ]}),
     )
     '''
-    actions = ['export_as_excel']
+    actions = ['export_excel']
 
     def typ(self,obj):
         return obj.serviceType.typ
@@ -44,10 +45,37 @@ class OrderAdmin(admin.ModelAdmin):
         return format_html(button_html)
     Photo.short_description = "图片"
 
-    def export_as_excel(self,request,queryset):
+    def export_excel(self,request,queryset):   
+        cols = ['序号','服务类别','维修内容','单价','提成率','技术等级']
         meta = self.model._meta
-        field_names = [field.name for field in meta.fields]
-        print(field_names)
+        response = HttpResponse(content_type='application/msexcel')
+        wb = Workbook()
+        ws = wb.active
+        ws.append(cols)
+        i = 1
+        for obj in queryset:
+            serviceType = obj.serviceType.typ
+            faultContent = obj.faultContent
+            add_up = obj.add_up
+            rate = obj.technician.level.rate
+            level = obj.technician.level.level
+            data = [i,serviceType,faultContent,add_up,rate,level]
+            row = ws.append(data)
+            i += 1
+        wb.save(response)
+        return response    
+        '''
+        content = {
+            'serviceType':self.serviceType.typ,
+            'faultContent':self.faultContent,
+            'add_up':self.add_up,
+            'rate':self.technician.level.rate,
+            'level':self.technician.level.level,
+        }
+        '''
+        for obj in queryset:
+            print(obj.serviceType.typ)
+
 admin.site.register(Client)
 admin.site.register(Order,OrderAdmin)
 admin.site.register(ServiceType)
