a23dd6e6af3b373734c42fdcc872a298c1ace00e
Robby <1260777400@qq.com>
2019-10-10 00:39:47 +0800

    logger debug

--- public/views.py
+++ public/views.py
@@ -104,6 +104,7 @@ def enrollScholarUser(request):
     openid = rspon.get_openid(app.appid,app.secret,content['code'])
     collect_logger.info('openid:'+openid)
     res_union = rspon.get_unionid(openid)
+    collect_logger.info('unionID'+str(res_union))
     divisions = divisionForm()
     context = {'openid':openid,'divisions':divisions}
     print(context)
